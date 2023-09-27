39b2885d6098e4d52e7422d940bca65b008b186c
Dertzey <djeketefra10@hotmail.com>
2023-09-27 20:04:25 +0200

    Fixed some minor issues

--- js/blogSpecific.js
+++ js/blogSpecific.js
@@ -1,21 +1,16 @@
 const QUERYSTRING = document.location.search;
 const PARAMS = new URLSearchParams(QUERYSTRING);
 const ID = PARAMS.get("id");
-const URLID = mainURL + '/' + ID; // Update the URL to include the specific post ID
+const URLID = mainURL + '/' + ID;
 
-let featuredImageURL = ''; // Initialize the variable
+let featuredImageURL = '';
 
 fetch(URLID)
     .then(response => response.json())
     .then(post => {
         const PRODUCTCONTAINER = document.querySelector(".blog-specific-container");
-
-        // Access the featured image URL from _embedded data
         const featuredImage = post._embedded && post._embedded['wp:featuredmedia'];
-
-        // Check if the featured image is available and get its source URL
         featuredImageURL = featuredImage ? featuredImage[0].source_url : '';
-
         PRODUCTCONTAINER.innerHTML = `
             <img class="blog-specific" src="${featuredImageURL}" alt="">
             <h1 class="blog-specific-title">${post.title.rendered}</h1>
--- js/carousel.js
+++ js/carousel.js
@@ -30,7 +30,7 @@ carousel.addEventListener("mousemove", (e) => {
     if (!isDragging) return;
     e.preventDefault();
     const x = e.pageX - carousel.offsetLeft;
-    const walk = (x - startX) * 2; // Adjust the multiplier for faster/slower scrolling
+    const walk = (x - startX) * 1;
     carousel.scrollLeft = scrollLeft - walk;
 });
 
