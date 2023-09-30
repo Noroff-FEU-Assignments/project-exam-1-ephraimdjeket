1478522697b0ce8191643f380a56f35c85072b0a
Dertzey <djeketefra10@hotmail.com>
2023-09-30 18:49:51 +0200

    Major changes and fix

--- js/blogSpecific.js
+++ js/blogSpecific.js
@@ -1,5 +1,4 @@
-import { mainURL } from '/js/blogFetch.js';
-
+const mainURL = 'https://cors.noroff.dev/dertzeydev.com/wp-json/wp/v2/posts';
 const QUERYSTRING = document.location.search;
 const PARAMS = new URLSearchParams(QUERYSTRING);
 const ID = PARAMS.get("id");
@@ -7,21 +6,31 @@ const URLID = mainURL + '/' + ID;
 
 let featuredImageURL = '';
 
-fetch(URLID)
-    .then(response => response.json())
-    .then(post => {
+async function fetchPostData() {
+    try {
+        const response = await fetch(URLID);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        document.getElementById('error-message-display').style.display = 'none';
+        const post = await response.json();
         const PRODUCTCONTAINER = document.querySelector(".blog-specific-container");
         const featuredImage = post._embedded && post._embedded['wp:featuredmedia'];
         featuredImageURL = featuredImage ? featuredImage[0].source_url : '';
         PRODUCTCONTAINER.innerHTML = `
             <img class="blogSpecific" src="${featuredImageURL}" alt="">
             <h1 class="blog-specific-title">${post.title.rendered}</h1>
-            <p class="blog-date">${post.date}</p>s
+            <p class="blog-date">${post.date}</p>
             <p class="blog-specific">${post.content.rendered}</p>
         </section>`;
         document.title = `${post.title.rendered}`;
-    })
-    .catch(error => {
-        console.log('An error occurred:', error);
-    });
+    } catch (error) {
+        console.error('An error occurred:', error);
+        document.getElementById('error-message-display').textContent = error.message;
+        document.getElementById('error-message-display').style.display = 'block';
+    }
+}
+
+fetchPostData();
+
 
--- js/loadMore.js
+++ js/loadMore.js
@@ -3,40 +3,40 @@ const CONTAINER = document.querySelector(".blog-post-cards");
 let currentPage = 1;
 const PER_PAGE = 10;
 
-function fetchPosts(page) {
+async function fetchPosts(page) {
     const fetchURL = `${mainURL}?page=${page}&per_page=${PER_PAGE}&_embed`;
 
-    fetch(fetchURL)
-        .then(response => {
-            if (!response.ok) {
+    try {
+        const response = await fetch(fetchURL);
+        if (!response.ok) {
+            throw new Error(`HTTP error :) Status: ${response.status}`);
+        }
+        document.getElementById('error-message-display').style.display = 'none';
+        const postsData = await response.json();
+        let blogPostCardsHTML = '';
 
-            }
-            return response.json();
-        })
-        .then(postsData => {
-            let blogPostCardsHTML = '';
+        postsData.forEach(post => {
+            const featuredMedia = post._embedded['wp:featuredmedia'][0];
+            const altText = featuredMedia.alt_text || '';
+            const imageUrl = featuredMedia.source_url;
 
-            postsData.forEach(post => {
-                const featuredMedia = post._embedded['wp:featuredmedia'][0];
-                const altText = featuredMedia.alt_text || '';
-                const imageUrl = featuredMedia.source_url;
-
-                blogPostCardsHTML += `
-                    <div class="blog-post-content">
-                        <img class="blog-postcard-img" src="${imageUrl}" alt="${altText}">
-                        <h3>${post.title.rendered}</h3>
-                        <p>${post.date}</p>
-                        <p class="blog-post-content-text">${post.excerpt.rendered}</p>
-                        <a href="blogspecific.html?id=${post.id}" class="read-more-btn">Read more</a>
-                    </div>`;
-            });
-
-            CONTAINER.innerHTML += blogPostCardsHTML;
-        })
-        .catch(error => {
-            console.log('An error occurred:', error);
-            document.querySelector('.load-more-btn').disabled = true;
+            blogPostCardsHTML += `
+                <div class="blog-post-content">
+                    <img class="blog-postcard-img" src="${imageUrl}" alt="${altText}">
+                    <h3>${post.title.rendered}</h3>
+                    <p>${post.date}</p>
+                    <p class="blog-post-content-text">${post.excerpt.rendered}</p>
+                    <a href="blogspecific.html?id=${post.id}" class="read-more-btn">Read more</a>
+                </div>`;
         });
+
+        CONTAINER.innerHTML += blogPostCardsHTML;
+    } catch (error) {
+        console.error('An error occurred:', error);
+        document.getElementById('error-message-display').textContent = error.message;
+        document.getElementById('error-message-display').style.display = 'block';
+        document.querySelector('.load-more-btn').disabled = true;
+    }
 }
 
 fetchPosts(currentPage);
@@ -45,3 +45,4 @@ document.querySelector('.load-more-btn').addEventListener('click', () => {
     currentPage++;
     fetchPosts(currentPage);
 });
+
